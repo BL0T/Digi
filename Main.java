9c707584903fd8bd011d84f274d8bfdbbb0c8857
BL0T <35530224+BL0T@users.noreply.github.com>
2023-01-13 23:48:05 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,62 @@
-import model.Animal;
-import service.AnimalService;
-
 public class Main {
+
     public static void main(String[] args) {
 
-        AnimalService service = new AnimalService();
+        Main m = new Main();
 
-        Animal a1 = service.crateAnimal();
-        Animal a2 = service.crateAnimal();
-        Animal a3 = service.crateAnimal();
+        for (int i = 0; i < 20; i++) {
+            System.out.println(m.faktorialFor(i) + " : " + m.faktorialRekursi(i));
+        }
 
-        service.printInfo(service.minAge(a1, a2, a3));
-        service.printInfo(service.maxAge(a1, a2, a3));
-        service.printInfo(service.maxWeight(a1, a2, a3));
-        service.printInfo(service.minWeight(a1, a2, a3));
-        service.printInfo(service.minAgePet(a1, a2, a3));
+        System.out.println();
+
+        for (int i = 0; i < 70; i++) {
+            System.out.println(m.fibonachiFor(i) + " : " + m.fibonachiRekursia(i));
+        }
+    }
+
+    public long faktorialFor(int n) {
+        if (n <= 0) return 0;
+
+        long x = 1;
+        for (int i = 2; i <= n; i++) {
+            x *= i;
+        }
+
+        return x;
+    }
+
+    public long faktorialRekursi(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        return n * faktorialRekursi(n - 1);
+    }
+
+    public long fibonachiFor(int n) {
+        if (n <= 0) return 0;
+        long x = 0;
+        long y = 1;
+        for (int i = 1; i < n; i++) {
+            y = x + y;
+            x = y - x;
+        }
+
+        return y;
+    }
+
+    public long fibonachiRekursia(int n) {
+        if (n <= 0) return 0;
+        if (n <= 2) return 1;
+        long[] arr = new long[n + 1];
+        arr[1] = 1;
+        arr[2] = 1;
+
+        return fibonachiRekursia(n, arr);
+    }
+
+    private long fibonachiRekursia(int n, long[] arr) {
+
+        return (arr[n - 1] != 0 ? arr[n - 1] : (arr[n - 1] = fibonachiRekursia(n - 1, arr))) +
+                (arr[n - 2] != 0 ? arr[n - 2] : (arr[n - 2] = fibonachiRekursia(n - 2, arr)));
     }
 }
