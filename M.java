943e474f1a74ea515416c1103ddb7cc78ed61c8d
BL0T <35530224+BL0T@users.noreply.github.com>
2023-01-20 23:41:57 +0400

    Add files via upload

--- M.java
+++ M.java
@@ -1,30 +1,45 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class M {
 
     public static void main(String[] args) throws IOException {
 
-        for (Student s : f()) {
-            System.out.println(s);
-        }
+        String url = "C:\\Users\\User\\Rekursia\\src\\test.txt";
+
+        write(url, minStudent(read(url)).toString());
     }
 
-    public static Student[] f() throws IOException {
+    public static Student[] read(String url) throws IOException {
 
-        Path path = Paths.get("C:\\Users\\User\\Rekursia\\src\\test.txt");
+        String[] strings = Files.readAllLines(Paths.get(url)).toArray(new String[0]);
+        Student[] students = new Student[strings.length];
 
-        Student[] students = new Student[11];
-
-        String[] strings = Files.readAllLines(path).toArray(new String[0]);
 
         for (int i = 0; i < strings.length; i++) {
-            String[] str = strings[i].split(",", 2);
+            String[] str = strings[i].split(",");
             students[i] = (new Student(str[0], Integer.parseInt(str[1])));
         }
 
         return students;
     }
+
+
+    public static Student minStudent(Student[] students) {
+        Student min = students[0];
+        for (int i = 1; i < students.length; i++) {
+            if (min.getAge() < students[i].getAge()) min = students[i];
+        }
+        return min;
+    }
+
+    public static void write(String url, String text) throws IOException {
+
+        Files.writeString(Paths.get(url), "\n" + text, StandardOpenOption.APPEND);
+
+
+    }
+
 }
